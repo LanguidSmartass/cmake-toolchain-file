595e56343d319c36e77d01be3c36ab11821d7d74
Novoselov Ivan <jedi.orden@gmail.com>
2022-05-31 21:17:28 +0400

    Add usage description in both README and toolchain-file.cmake comments

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # cmake-toolchain-file
-A CMake toolchain file for manual toolchain selection
+
+Configure custom C/C++ toolchain, particularly useful in embedded development
+
+```shell
+# Usage:
+# Pass these three options to cmake options (through terminal/console/IDE)
+# Setting them in scripts won't take effect you desire
+
+# call to executable with arguments passed below:
+cmake 
+# arg, path to the toolchain file from this repo
+--toolchain "cmake-toolchain-file/toolchain-file.cmake"
+# arg, toolchain prefix like "avr-", "arm-none-eabi-", etc.
+#      can be just an empty string ""
+-Dcmake-toolchain-prefix="toolchain-prefix"
+# arg, full path to your toolchain
+-Dcmake-toolchain-root:PATH="/path/to/your/toolchain"
+# Also, '--toolchain' and '-DCMAKE_TOOLCHAIN_FILE=' are equivalent
+# cmake option expressions
+```
--- toolchain-file.cmake
+++ toolchain-file.cmake
@@ -1,3 +1,32 @@
+# toolchain-file.cmake
+#
+# @brief toolchain file for cmake options
+#
+# @author Ivan Novoselov, jedi.orden@gmail.com
+#
+# @date Apr 10, 2019
+#
+# @details
+# Configure custom C/C++ toolchain, particularly useful in embedded development
+#
+# Usage:
+# Pass these three options to cmake options (through terminal/console/IDE)
+# Setting them in scripts won't take effect you desire
+#
+# # call to executable with arguments passed below:
+# cmake
+# # arg, path to the toolchain file from this repo
+# --toolchain "cmake-toolchain-file/toolchain-file.cmake"
+# # arg, toolchain prefix like "avr-", "arm-none-eabi-", etc.
+# #      can be just an empty string ""
+# -Dcmake-toolchain-prefix="toolchain-prefix"
+# # arg, full path to your toolchain
+# -Dcmake-toolchain-root:PATH="/path/to/your/toolchain"
+#
+# Also, '--toolchain' and '-DCMAKE_TOOLCHAIN_FILE=' are equivalent
+# cmake option expressions
+
+
 if(NOT $ENV{cmake-toolchain-root})
     message(WARNING "To use this toolchain file you should specify 'cmake-toolchain-root' variable")
 endif()
