335736b0673c4555fcfb5c0f323504e7be2b09fd
Novoselov Ivan <jedi.orden@gmail.com>
2022-05-31 20:27:36 +0400

    Cleanup the script, make it more concice and readable

--- toolchain-file.cmake
+++ toolchain-file.cmake
@@ -1,5 +1,3 @@
-set(binutils objcopy objdump size)
-
 if(NOT $ENV{cmake-toolchain-root})
     message(WARNING "To use this toolchain file you should specify 'cmake-toolchain-root' variable")
 endif()
@@ -8,42 +6,51 @@ if(NOT $ENV{cmake-toolchain-prefix})
     message(WARNING "You should probably define a 'cmake-toolchain-prefix' variable as well")
 endif()
 
+
 if(${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows")
-    set(exec_end ".exe")
-elseif(${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Linux")
-    set(exec_end "")
+
+    set(CMAKE_FIND_ROOT_PATH  ${cmake-toolchain-root})
+    set(ext-exe ".exe")
+
+elseif(
+       (${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Linux")
+    OR (${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Datwin")
+)
+
+    set(CMAKE_FIND_ROOT_PATH  ${cmake-toolchain-root})
+    set(ext-exe "")
+
 else()
-    message("${CMAKE_HOST_SYSTEM_NAME}" not supported yet)
+
+    message(FATAL_ERROR "Unknown host system name: ${CMAKE_HOST_SYSTEM_NAME}")
+
 endif()
 
+
 # Set some helper variables, I'll get rid of them later with 'unset'
-set(ar      ${cmake-toolchain-prefix}ar${exec_end})
-set(ccomp   ${cmake-toolchain-prefix}gcc${exec_end})
-set(cxxcomp ${cmake-toolchain-prefix}g++${exec_end})
-set(cxxfilt ${cmake-toolchain-prefix}c++filt${exec_end})
-set(gdb     ${cmake-toolchain-prefix}gdb${exec_end})
-set(nm      ${cmake-toolchain-prefix}nm${exec_end})
-set(objcopy ${cmake-toolchain-prefix}objcopy${exec_end})
-set(objdump ${cmake-toolchain-prefix}objdump${exec_end})
-set(readelf ${cmake-toolchain-prefix}readelf${exec_end})
-set(size    ${cmake-toolchain-prefix}size${exec_end})
+set(ar      ${cmake-toolchain-prefix}ar${ext-exe})
+set(ccomp   ${cmake-toolchain-prefix}gcc${ext-exe})
+set(cxxcomp ${cmake-toolchain-prefix}g++${ext-exe})
+set(cxxfilt ${cmake-toolchain-prefix}c++filt${ext-exe})
+set(gdb     ${cmake-toolchain-prefix}gdb${ext-exe})
+set(nm      ${cmake-toolchain-prefix}nm${ext-exe})
+set(objcopy ${cmake-toolchain-prefix}objcopy${ext-exe})
+set(objdump ${cmake-toolchain-prefix}objdump${ext-exe})
+set(readelf ${cmake-toolchain-prefix}readelf${ext-exe})
+set(size    ${cmake-toolchain-prefix}size${ext-exe})
+
 
 #set(CMAKE_CROSSCOMPILING   TRUE)
-# this is a name of a target system, not a host one (which I compile and debug from)
+# this is a name of a target system,
+# not a host on which the cmake and build takes place
 set(CMAKE_SYSTEM_NAME      Generic)
+
 
 # without setting CMAKE_TRY_COMPILE_TARGET_TYPE to STATIC_LIBRARY
 # the CMake try_compile the none 'main' function will fail on
 # a linking error like 'undefined reference to '_exit'
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
-if(${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows")
-    set(CMAKE_FIND_ROOT_PATH  ${cmake-toolchain-root})
-elseif(${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Linux")
-    set(CMAKE_FIND_ROOT_PATH  ${cmake-toolchain-root})
-else()
-    message("${CMAKE_HOST_SYSTEM_NAME}" not supported yet)
-endif()
 
 set(CMAKE_C_COMPILER      ${CMAKE_FIND_ROOT_PATH}/bin/${ccomp})
 set(CMAKE_CXX_COMPILER    ${CMAKE_FIND_ROOT_PATH}/bin/${cxxcomp})
@@ -59,11 +66,14 @@ set(CMAKE_OBJDUMP   ${CMAKE_FIND_ROOT_PATH}/bin/${objdump})
 set(CMAKE_READELF   ${CMAKE_FIND_ROOT_PATH}/bin/${readelf})
 set(CMAKE_SIZE      ${CMAKE_FIND_ROOT_PATH}/bin/${size})
 
+
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
+
+# Cleanup
 unset(ccomp)
 unset(cxxcomp)
 unset(cxxfilt)
@@ -74,6 +84,3 @@ unset(objdump)
 unset(size)
 unset(gdb)
 unset(readelf)
-
-unset(cmake-toolchain-prefix)
-unset(cmake-toolchain-root)
